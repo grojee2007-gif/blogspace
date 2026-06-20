13adc490bd0e40e0bf9c9b1407b9a251fe553e08
Rojee <24r11a62b2@gcet.edu.in>
2026-06-20 21:43:54 +0530

    fix package.json

--- backend/server.js
+++ backend/server.js
@@ -17,4 +17,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log("Server running"));
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
