195da87807ab56b7e1c406b99d0bcc207230402b
Rojee <24r11a62b2@gcet.edu.in>
2026-06-20 22:16:55 +0530

    add test route

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log("MongoDB ERROR:", err));
+  .catch(err => console.log(err));
 
+// ROOT ROUTE
 app.get("/", (req, res) => {
   res.send("Backend Running");
+});
+
+// 👇 ADD THIS HERE
+app.get("/test", (req, res) => {
+  res.json({ message: "Test route working 🚀" });
 });
 
 const PORT = process.env.PORT || 8080;
