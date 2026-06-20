b8bfcd861f3436017a320b50d04dc2566c1a84a5
Rojee <24r11a62b2@gcet.edu.in>
2026-06-20 20:10:19 +0530

    fix mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv");
-
-dotenv.config();
-
-const connectDB = require("./config/db");
-
-// Connect MongoDB
-connectDB();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/posts", require("./routes/postRoutes"));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log("MongoDB ERROR:", err));
 
-// Server
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Backend Running");
 });
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log("Server running"));
